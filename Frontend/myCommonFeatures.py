2838ed0365be02a9cc2f15d09ed805c67028529e
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-02-06 20:19:05 +0200

    Added first steps in design development

---
+++ Frontend/myCommonFeatures.py
@@ -0,0 +1,11 @@
+'''
+This module contains log, path \n
+'''
+from rich.console import Console
+import os
+
+log = Console().log
+'''Function for logging in console'''
+
+path = os.getcwd() + "\\"
+'''String with path to program folder (...\\TR_Chats\\Frontend\\ )'''
---
+++ Frontend/webserver.py
@@ -0,0 +1,38 @@
+"""Main module that launch webserver based on Flask"""
+# other libs
+from flask import Flask, render_template, request, send_file, make_response
+# my libs
+from myCommonFeatures import log, path
+
+app = Flask("TR chat front")
+
+
+def generateResponse(data):
+    """Returns response with data"""
+    response = make_response(data)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
+
+@app.route("/")
+def index():
+    '''Renders index page on server'''
+    return render_template("index.html")
+
+
+@app.route("/resource")
+def get_resource():
+    '''Gives files to server'''
+    file_name = request.args.get("name")
+    file_path = path + "resources\\" + file_name
+
+    try:
+        resource = send_file(file_path)
+        log(f"File [green]{file_name}[/green] is loaded! ")
+        return resource
+    except:
+        log(f"File [red]{file_name}[/red] not founded :(")
+        return generateResponse("")
+
+
+app.run(host="0.0.0.0", port=8080, debug=True)
