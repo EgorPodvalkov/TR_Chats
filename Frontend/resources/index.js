faba10d481363a0c375fa8d7108e87b5158494d3
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-02-16 17:22:06 +0200

    Added channel and send panels

--- Frontend/resources/index.js
+++ Frontend/resources/index.js
@@ -1,9 +1,8 @@
-function addSomeThingToWorkplaces(){
+function add_workplace(name = "New Workplace"){
     workplace = document.getElementsByClassName("workplaces_area")[0];
-    workplace.innerHTML += "<button>new workplace</button>"
+    workplace.innerHTML += "<button>" + name + "</button>";
 }
-addSomeThingToWorkplaces()
-
+add_workplace("Test")
 function show_workplaces_adder(bool = true){
     if(bool){
         document.getElementsByClassName("adder_workplace")[0].style = "display: inline;"
@@ -21,4 +20,4 @@ function show_workplaces_adder(bool = true){
 
 
 
-// setTimeout(() => {location.reload()}, 1000)
+setTimeout(() => {location.reload()}, 1000)
