0c82e29a9628520fa039c15e80c55ff2d515c60f
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-02-18 19:52:58 +0200

    Added functions to menage workplaces and chanels tables and fixed one lil naming mistake

--- Backend/database.py
+++ Backend/database.py
@@ -31,11 +31,37 @@ def initConnection(nameDB: str):
 
 
 def initTables(connection: str):
-    '''Tries to create new tables for users, verefication'''
+    '''Tries to create new tables for users, verefication and workplaces'''
     sql = "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, login TEXT NOT NULL, nickname TEXT NOT NULL, password TEXT NOT NULL, authentication_method TEXT NOT NULL, authentication_name TEXT NOT NULL, session_code TEXT NOT NULL)"
     connection.execute(sql)
     sql = "CREATE TABLE IF NOT EXISTS verification( id INTEGER PRIMARY KEY, authentication_name TEXT NOT NULL, verification_code TEXT)"
     connection.execute(sql)
+    sql = "CREATE TABLE IF NOT EXISTS workplaces( id INTEGER PRIMARY KEY, workplace_name TEXT NOT NULL, creator_name TEXT NOT NULL)"
+    connection.execute(sql)
+    log("Tables [blue]users[/blue], [blue]verification[/blue], [blue]workplaces[/blue] created (if they not exist)!")
+
+
+
+def initUserWorkplacesTable(nameDB: str, user_id: str):
+    '''Creates new table with all workplaces of user'''
+    connection = initConnection(nameDB)
+    sql = f"CREATE TABLE IF NOT EXISTS user{user_id}_workplaces( id INTEGER PRIMARY KEY, global_id TEXT NOT NULL, workplace_name TEXT NOT NULL)"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    connection.commit()
+    connection.close()
+    log(f"Table [blue]user{user_id}_workplaces[/blue] created!")
+
+
+def initWorkplaceChanelsTable(nameDB: str, workplace_id: str):
+    '''Creates new table with all workplaces of user'''
+    connection = initConnection(nameDB)
+    sql = f"CREATE TABLE IF NOT EXISTS workplace{workplace_id}_chanels( id INTEGER PRIMARY KEY, name TEXT NOT NULL, chat TEXT)"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    connection.commit()
+    connection.close()
+    log(f"Table [blue]workplace{workplace_id}_chanels[/blue] created!")
 
 
 # change data block
@@ -63,9 +89,49 @@ def addUser(nameDB: str, login: str, nickname: str, password: str, authenticatio
         connection.commit()
         connection.close()
         log(f"User [yellow]{login}[/yellow] added to users table")
+
+        initUserWorkplacesTable(nameDB, getUserInfoByLogin(nameDB, login, "id"))
+
         return 'Added'
     else:
         return valid
+
+
+def addWorkplace(nameDB: str, workplace_name: str, creator_name: str):
+    """Adds workplace to workplaces table"""
+    
+    # workplace name checking
+    if workplace_name in getWorkplaceNames(nameDB):
+        return "Workplace name is already taken"
+
+    connection = initConnection(nameDB)
+    sql = f"INSERT INTO worplaces(`workplace_name`, `creator_name`) VALUES('{workplace_name}', '{creator_name}')"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    connection.commit()
+    connection.close()
+    log(f"Workplace [yellow]{workplace_name}[/yellow] added to workplaces table")
+
+    global_id = getIdByWorkplaceName(nameDB, workplace_name)
+    initWorkplaceChanelsTable(nameDB, global_id)
+
+    addUserToWorkplace(nameDB, workplace_name, getUserInfoByLogin(nameDB, creator_name, "id"))
+
+    
+
+
+def addUserToWorkplace(nameDB: str, workplace_name: str, user_id: str):
+    """Adds workplace to specified user_workplaces table"""
+
+    global_id = getIdByWorkplaceName(nameDB, workplace_name)
+
+    connection = initConnection(nameDB)
+    sql = f"INSERT INTO user{user_id}_workplaces(`global_id`, `workplace_name`) VALUES('{global_id}', '{workplace_name}')"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    connection.commit()
+    connection.close()
+    log(f"Workplace [yellow]{workplace_name}[/yellow] added to user{user_id}_workplaces table")
 
 
 def updateVerificationCode(nameDB: str, authentication_name: str, verification_code: str):
@@ -120,26 +186,26 @@ def userValidation(nameDB: str, login: str, authentication_name: str) -> str:
 
 def getUsersColumn(nameDB: str, column: str) -> list[str]:
     """Returns list of specified column\n
-    Columns: 'login', 'nickname', 'password', 'authentication_method', 'authentication_name', 'session_code'"""
+    Columns: 'id', 'login', 'nickname', 'password', 'authentication_method', 'authentication_name', 'session_code'"""
     connection = initConnection(nameDB)
     sql = f"SELECT {column} FROM users"
     cursor = connection.cursor()
     cursor.execute(sql)
     rows = cursor.fetchall()
     connection.close()        
-    return [row[0] for row in rows]
+    return [str(row[0]) for row in rows]
 
 
 def getUserInfoByLogin(nameDB: str, login:str, column: str) -> str:
     """Returns specified info by user login\n
-    Columns: 'login', 'nickname', 'password', 'authentication_method', 'authentication_name', 'session_code'"""
+    Columns: 'id', 'login', 'nickname', 'password', 'authentication_method', 'authentication_name', 'session_code'"""
     connection = initConnection(nameDB)
     sql = f"SELECT {column} FROM users WHERE login = '{login}'"
     cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()[0][0]
     connection.close()
-    return result
+    return str(result)
 
 
 def getLoginBySession(nameDB: str, session_code:str):
@@ -178,8 +244,27 @@ def getVerificationCode(nameDB: str, authentication_name:str) -> str:
     connection.close()
     return result
 
+    # workplace
+def getWorkplaceNames(nameDB: str) -> list[str]:
+    """Returns list of workplace names"""
+    connection = initConnection(nameDB)
+    sql = f"SELECT workplace_name FROM workplaces"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    connection.close()        
+    return [row[0] for row in rows]
 
 
+def getIdByWorkplaceName(nameDB: str, workplace_name:str) -> str:
+    """Returns id by workplace_name from workplaces table"""
+    connection = initConnection(nameDB)
+    sql = f"SELECT id FROM workplaces WHERE workplace_name = '{workplace_name}'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()[0][0]
+    connection.close()
+    return str(result)
 
 
 # nameDB = "TRChat.db"
